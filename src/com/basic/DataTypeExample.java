daa2dd1e9471e6b66b6d2b26e72930d3f23f885c
Elakkiyaanu <elakkiyalea@gmail.com>
2018-05-29 16:09:17 +0530

    string example

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class DataTypeExample 
+{
+	public static void main(String []args)
+{
+	String firstName="Elakkiya";
+	String lastName="Elangovan";
+	System.out.println(firstName+" "+lastName);
+}
+
+}
